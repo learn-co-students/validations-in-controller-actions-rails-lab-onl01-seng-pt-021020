e23d789c0fb13621202a7ac39eb207659326d423
pbedrosian <peter_bedrosian@Yahoo.com>
2020-06-23 19:37:35 -0400

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,15 +1,27 @@
 class AuthorsController < ApplicationController
+
+  def index 
+    @authors = Author.all
+  end
+
   def show
     @author = Author.find(params[:id])
   end
 
   def new
+    @author = Author.new
   end
 
   def create
-    @author = Author.create!(author_params)
+    @author = Author.new(author_params)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+      # format.html { redirect_to @author, notice: 'Author was successfully created.' }
+    else 
+      render :new
+    end
 
-    redirect_to author_path(@author)
   end
 
   private
